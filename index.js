e109ee3c57592149b6347c91641291ae7d2714c0
Mery Fajardo <meryfajardo93@gmail.com>
2023-02-17 15:07:50 -0500

    last changes

--- index.js
+++
@@ -1,6 +0,0 @@
-const nav_toogle = document.querySelector(".toggle")
-const menu = document.querySelector(".nav-list")
-
-toogle.addEventListener("click", () => {
-    menu.classList.toogle("nav-list_visible")
-})
